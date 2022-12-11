5ec6b6777e68d4c063f100183cde71341efdac77
OliverFang™ <khaizix123@gmail.com>
2022-12-11 11:12:49 +0700

    first commit

---
+++ starterChapter11/script.js
@@ -0,0 +1,385 @@
+'use strict';
+
+/////////////////////////////////////////////////
+/////////////////////////////////////////////////
+// BANKIST APP
+
+/////////////////////////////////////////////////
+// Data
+
+// DIFFERENT DATA! Contains movement dates, currency and locale
+
+const account1 = {
+  owner: 'Jonas Schmedtmann',
+  movements: [200, 455.23, -306.5, 25000, -642.21, -133.9, 79.97, 1300],
+  interestRate: 1.2, // %
+  pin: 1111,
+
+  movementsDates: [
+    '2019-11-18T21:31:17.178Z',
+    '2019-12-23T07:42:02.383Z',
+    '2020-01-28T09:15:04.904Z',
+    '2020-04-01T10:17:24.185Z',
+    '2020-05-08T14:11:59.604Z',
+    '2020-05-27T17:01:17.194Z',
+    '2022-12-05T23:36:17.929Z',
+    '2022-12-10T10:51:36.790Z',
+  ],
+  currency: 'EUR',
+  locale: 'pt-PT', // de-DE
+};
+
+const account2 = {
+  owner: 'Jessica Davis',
+  movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
+  interestRate: 1.5,
+  pin: 2222,
+
+  movementsDates: [
+    '2019-11-01T13:15:33.035Z',
+    '2019-11-30T09:48:16.867Z',
+    '2019-12-25T06:04:23.907Z',
+    '2020-01-25T14:18:46.235Z',
+    '2020-02-05T16:33:06.386Z',
+    '2020-04-10T14:43:26.374Z',
+    '2020-06-25T18:49:59.371Z',
+    '2020-07-26T12:01:20.894Z',
+  ],
+  currency: 'USD',
+  locale: 'en-US',
+};
+
+const accounts = [account1, account2];
+
+/////////////////////////////////////////////////
+// Elements
+const labelWelcome = document.querySelector('.welcome');
+const labelDate = document.querySelector('.date');
+const labelBalance = document.querySelector('.balance__value');
+const labelSumIn = document.querySelector('.summary__value--in');
+const labelSumOut = document.querySelector('.summary__value--out');
+const labelSumInterest = document.querySelector('.summary__value--interest');
+const labelTimer = document.querySelector('.timer');
+
+const containerApp = document.querySelector('.app');
+const containerMovements = document.querySelector('.movements');
+
+const btnLogin = document.querySelector('.login__btn');
+const btnTransfer = document.querySelector('.form__btn--transfer');
+const btnLoan = document.querySelector('.form__btn--loan');
+const btnClose = document.querySelector('.form__btn--close');
+const btnSort = document.querySelector('.btn--sort');
+
+const inputLoginUsername = document.querySelector('.login__input--user');
+const inputLoginPin = document.querySelector('.login__input--pin');
+const inputTransferTo = document.querySelector('.form__input--to');
+const inputTransferAmount = document.querySelector('.form__input--amount');
+const inputLoanAmount = document.querySelector('.form__input--loan-amount');
+const inputCloseUsername = document.querySelector('.form__input--user');
+const inputClosePin = document.querySelector('.form__input--pin');
+
+/////////////////////////////////////////////////
+// Functions
+
+const formatMovementsDate = function (date) {
+  const calcDisplayDate = (day1, day2) =>
+    Math.round(Math.abs((day2 - day1) / (24 * 60 * 60 * 1000)));
+
+  const substractDate = calcDisplayDate(new Date(), date);
+
+  if (substractDate === 0) return 'Today';
+  if (substractDate === 1) return 'Yesterday';
+  if (substractDate <= 7) return `${substractDate} days`;
+
+  const year = date.getFullYear();
+  const month = `${date.getMonth() + 1}`.padStart(2, 0);
+  const day = `${date.getDate()}`.padStart(2, 0);
+
+  return `${year}/${month}/${day}`;
+};
+
+const displayMovements = function (accs, sort = false) {
+  containerMovements.innerHTML = '';
+
+  const movs = sort
+    ? accs.movements.slice().sort((a, b) => a - b)
+    : accs.movements;
+
+  movs.forEach(function (mov, i) {
+    const type = mov > 0 ? 'deposit' : 'withdrawal';
+    const getDate = new Date(accs.movementsDates[i]);
+
+    const displayDate = formatMovementsDate(getDate);
+
+    const html = `
+      <div class="movements__row">
+        <div class="movements__type movements__type--${type}">${
+      i + 1
+    } ${type}</div>
+        <div class="movements__date">${displayDate}</div>
+        <div class="movements__value">${mov}€</div>
+      </div>
+    `;
+
+    containerMovements.insertAdjacentHTML('afterbegin', html);
+  });
+};
+
+const calcDisplayBalance = function (acc) {
+  acc.balance = acc.movements.reduce((acc, mov) => acc + mov, 0);
+  labelBalance.textContent = `${acc.balance}€`;
+};
+
+const calcDisplaySummary = function (acc) {
+  const incomes = acc.movements
+    .filter(mov => mov > 0)
+    .reduce((acc, mov) => acc + mov, 0);
+  labelSumIn.textContent = `${incomes}€`;
+
+  const out = acc.movements
+    .filter(mov => mov < 0)
+    .reduce((acc, mov) => acc + mov, 0);
+  labelSumOut.textContent = `${Math.abs(out)}€`;
+
+  const interest = acc.movements
+    .filter(mov => mov > 0)
+    .map(deposit => (deposit * acc.interestRate) / 100)
+    .filter((int, i, arr) => {
+      // console.log(arr);
+      return int >= 1;
+    })
+    .reduce((acc, int) => acc + int, 0);
+  labelSumInterest.textContent = `${interest}€`;
+};
+
+const createUsernames = function (accs) {
+  accs.forEach(function (acc) {
+    acc.username = acc.owner
+      .toLowerCase()
+      .split(' ')
+      .map(name => name[0])
+      .join('');
+  });
+};
+createUsernames(accounts);
+
+const updateUI = function (acc) {
+  // Display movements
+  displayMovements(acc);
+
+  // Display balance
+  calcDisplayBalance(acc);
+
+  // Display summary
+  calcDisplaySummary(acc);
+};
+
+///////////////////////////////////////
+// Event handlers
+let currentAccount;
+
+btnLogin.addEventListener('click', function (e) {
+  // Prevent form from submitting
+  e.preventDefault();
+
+  currentAccount = accounts.find(
+    acc => acc.username === inputLoginUsername.value
+  );
+  console.log(currentAccount);
+
+  if (currentAccount?.pin === Number(inputLoginPin.value)) {
+    // Display UI and message
+    labelWelcome.textContent = `Welcome back, ${
+      currentAccount.owner.split(' ')[0]
+    }`;
+    containerApp.style.opacity = 100;
+
+    //get Date
+    const curTime = new Date();
+    const year = curTime.getFullYear();
+    const month = `${curTime.getMonth() + 1}`.padStart(2, 0);
+    const day = `${curTime.getDate()}`.padStart(2, 0);
+    const hours = `${curTime.getHours()}`.padStart(2, 0);
+    const minutes = `${curTime.getMinutes()}`.padStart(2, 0);
+    labelDate.textContent = `${day}/${month}/${year}, ${hours}:${minutes}`;
+
+    // Clear input fields
+    inputLoginUsername.value = inputLoginPin.value = '';
+    inputLoginPin.blur();
+
+    // Update UI
+    updateUI(currentAccount);
+  }
+});
+
+btnTransfer.addEventListener('click', function (e) {
+  e.preventDefault();
+  const amount = Number(inputTransferAmount.value);
+  const receiverAcc = accounts.find(
+    acc => acc.username === inputTransferTo.value
+  );
+  inputTransferAmount.value = inputTransferTo.value = '';
+
+  if (
+    amount > 0 &&
+    receiverAcc &&
+    currentAccount.balance >= amount &&
+    receiverAcc?.username !== currentAccount.username
+  ) {
+    // Doing the transfer
+    currentAccount.movements.push(-amount);
+    receiverAcc.movements.push(amount);
+
+    //Doing updating Date
+    currentAccount.movementsDates.push(new Date().toISOString());
+    receiverAcc.movementsDates.push(new Date().toISOString());
+
+    // Update UI
+    updateUI(currentAccount);
+  }
+});
+
+btnLoan.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  const amount = Number(inputLoanAmount.value);
+
+  if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
+    // Add movement
+    currentAccount.movements.push(amount);
+
+    //updating Date
+    currentAccount.movementsDates.push(new Date().toISOString());
+
+    // Update UI
+    updateUI(currentAccount);
+  }
+  inputLoanAmount.value = '';
+});
+
+btnClose.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  if (
+    inputCloseUsername.value === currentAccount.username &&
+    Number(inputClosePin.value) === currentAccount.pin
+  ) {
+    const index = accounts.findIndex(
+      acc => acc.username === currentAccount.username
+    );
+    console.log(index);
+    // .indexOf(23)
+
+    // Delete account
+    accounts.splice(index, 1);
+
+    // Hide UI
+    containerApp.style.opacity = 0;
+  }
+
+  inputCloseUsername.value = inputClosePin.value = '';
+});
+
+let sorted = false;
+btnSort.addEventListener('click', function (e) {
+  e.preventDefault();
+  displayMovements(currentAccount, !sorted);
+  sorted = !sorted;
+});
+
+//Display Account1 Default
+currentAccount = account1;
+updateUI(currentAccount);
+containerApp.style.opacity = 100;
+
+/////////////////////////////////////////////////
+/////////////////////////////////////////////////
+// LECTURES
+
+//---------------------------------Number
+// 1.isFinite() and Number.isFinite() : Kiểm tra số hữu hạn
+/** isFinite() là global function, kiểm tra giá trị đã cho có phải 1 không,
+ * nếu cần convert đối số truyền vào thành number
+ * Number.isFinite() cũng kiểm tra giống isFinite() tuy nhiên ko convert đối số thành number
+ */
+console.log(Number.isFinite(3e64));
+console.log(isFinite(3e64));
+
+console.log(Number(null));
+
+// 2.Number.isNaN(): giá trị trả về true nếu đối số có giá trị là NaN, ngược lại là false
+console.log(Number.isNaN(NaN));
+console.log(Number.isNaN(0 / 0));
+console.log(Number.isNaN('37'));
+console.log(Number.isNaN(37));
+
+// 3.Number.isInteger() : kiếm tra giá trị truyền vào là 1 số nguyên
+console.log(Number.isInteger(0.1));
+console.log(Number.isInteger(Math.PI));
+
+// 4.Number.parseFloat() : Phân tích 1 đối số truyền vào và trả về số thập phân
+/** đối số được truyền vào là kiểu String ---- Number.parseFloat(string)
+ * khoảng trắng trong dối số được bỏ qua
+ * trả về NaN nếu kí tự đầu tiên trong đối số ko thể convert thành number
+ */
+
+console.log(Number.parseFloat('1abc'));
+console.log(Number.parseFloat('a1bc'));
+
+// Number.prototye.toFixed() : Định dạng 1 số sử dụng ký hiệu đếm cố định
+/**phương thức có 1 tham số digits đại diện cho số chữ số xuất hiện sau dấu thập phân
+ * nếu digits <0 và > 100 và = NaN sẽ trả về lỗi
+ * kiểu dữ liệu trả về của methods là string
+ */
+const numObj = 12345.6789;
+console.log(numObj.toFixed(2));
+console.log(numObj.toFixed(6));
+console.log((2.34).toFixed(1));
+console.log((2.35).toFixed(1));
+console.log((2.65).toFixed(1));
+console.log(typeof -(2.34).toFixed(1));
+console.log(typeof (-2.34).toFixed(1));
+console.log('---------------------------------------------------');
+
+//---------------------------------Date
+//Create a date :
+/*
+const now = new Date();
+console.log(now);
+
+console.log(new Date('Sun Dec 11 2022 08:36:37'));
+console.log(new Date('December 16, 2022')); // do not create this because it can be unreliable
+console.log(new Date(account1.movementsDates[0]));
+
+// year - month - day - hour - minutes - second
+// month được bắt đầu từ vị trí 0 nên month = 10 means November (Tháng 11)
+console.log(new Date(2037, 10, 19, 15, 23, 5));
+
+//Nếu tháng đó (Tháng 11) có tối đa 30 ngày mà giá trị truyền vào vượt quá con số đó
+//nó sẽ lập tức được nhảy sang tháng tiếp theo bắt đầu = ngày (value - ngày tối đa của tháng)
+
+console.log(new Date(2037, 10, 32));
+
+// The amount of milliseconds passed since the beginning of the Unix time(January 1st 1970)
+// Công thức chuyển đổi ngày sang millisecons (hour * minute * second * 1000), được gọi là timestamp
+console.log(new Date(0));
+*/
+
+//Working with dates : có thể sử dụng được get-set
+/*
+const future = new Date(2037, 10, 19, 15, 23);
+
+console.log(future.getFullYear());
+console.log(future.getMonth());
+console.log(future.getDate());
+console.log(future.getDay()); // trả về giá trị với thứ tự của Thứ nằm trong tuần (Ex: Thứ 5 -> 4)
+console.log(future.toISOString());
+console.log(future.getTime()); // get Timestamp
+console.log(Date.now()); // trả về dạng Timestamp
+console.log(new Date().toISOString()); // trả về IOS format
+*/
+
+/*setTimeout() simply schedule a function to run after a certain amout of time but
+the callback function() is only excuted once
+*/
+//clearTimeout() : clear the time out and delete the timer
